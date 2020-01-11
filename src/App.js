84e8708cdce3f49ef7b5aabda6c2a96c4da7e8ea
KwangKyunBae <bae7359721@naver.com>
2020-01-11 17:12:24 +0900

    완성

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
 
   render(){
     return(
-      <div className="App">
+      <div className={this.state.movies ? "App" : "App-loading"}>
         {this.state.movies ? this.__renderMovies() : 'Loading'}
         
       </div>
