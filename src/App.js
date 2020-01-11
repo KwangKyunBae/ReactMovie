95f813ed8390de6483c750b4c9fca7359852e6ab
KwangKyunBae <bae7359721@naver.com>
2020-01-11 15:17:56 +0900

    영화API불러오기,fetch

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ class App extends Component {
     
   }
   componentDidMount(){
-    console.log(fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating'))
+    fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
+    .then(potato => potato.json())
+    .then(json => console.log(json)) //fetch의 결과물
+    .catch(err => console.log(err))
   }
 
   __renderMovies = () =>{
